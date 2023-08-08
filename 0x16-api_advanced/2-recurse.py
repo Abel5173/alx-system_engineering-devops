7655bca95204da28bdb4399a6012a0c2ba3e9323
Abel <86670708+Abel5173@users.noreply.github.com>
2023-08-08 23:52:24 +0300

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-"""
-Recursive function that queries the Reddit API.
-Returns a list containing the titles of all hot articles for a given subreddit.
-"""
+""" Exporting csv files"""
+import json
 import requests
+import sys
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """queries reddit API"""
-    if subreddit is None or type(subreddit) is not str:
-        return None
-
-    url = 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'ALX-advanced_APIi/by Ajiboye Adeleye pius'}
-    params = {
-            'after': after,
-            'limit': 100
-            }
-    resp = requests.get(url, headers=headers, params=params,
-                        allow_redirects=True)
-    if resp.status_code == 404:
-        return None
-
-    res = resp.json().get('data')
-    after = res.get('after')
-    count = res.get('dist')
-    for item in res.get('children'):
-        hot_list.append(item.get('data').get('title'))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after)
-    return hot_list
+def recurse(subreddit, host_list=[], after="null"):
+    """Read reddit API and return top 10 hotspots """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    payload = {"limit": "100", "after": after}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False, params=payload)
+    if r.status_code == 200:
+        list_titles = r.json()['data']['children']
+        after = r.json()['data']['after']
+        if after is not None:
+            host_list.append(list_titles[len(host_list)]['data']['title'])
+            recurse(subreddit, host_list, after)
+        else:
+            return(host_list)
+    else:
+        return(None)
